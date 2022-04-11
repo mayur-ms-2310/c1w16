57d7fd97eb6d7f8bbdb4d5a5ecdd063dad669526
nimishshinde <mayurshinde2310@gmail.com>
2022-04-11 13:41:57 +0530

    update

--- src/components/Grocerydetails.jsx
+++ src/components/Grocerydetails.jsx
@@ -5,14 +5,13 @@ import CartButton from "./CartButton"
 const GroceryDetails = ()=>{
 console.log(data)
 
-
     return(
         <>
         <h1>Groceries</h1>
         <div className="container" style={{display:"grid", gridTemplateColumns:"repeat(5,20%)"}}>
             {/* map through the data and display the cards */}
         {data.map((item)=>{
-            return <><div ><GroceryItem {...item}/>
+            return <><div><GroceryItem {...item}/>
            <CartButton {...item}/></div></>
         })}
         </div>
